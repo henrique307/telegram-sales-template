270e171a095b4664dcf3ef8ca9fca6305389f35c
henrique <henrique.florencio307@gmail.com>
2023-12-09 18:17:00 -0300

    adicionando assets na dist em tempo de compilação

--- src/temp/temp.storage.ts
+++ src/temp/temp.storage.ts
@@ -6,7 +6,7 @@ export const produtos: Produto[] = [
       nome: 'VIP 1',
       descricao: 'Grupo 1 - 30 vídeos e 40 fotos ',
       preco: 'R$14.90',
-      imagem: `${__dirname}\\..\\..\\assets/vip1.jpg`,
+      imagem: `${__dirname}\\..\\assets\\vip1.jpg`,
       linkPagamento: "https://pepper.com.br/checkout/index.html?p=100009&o=116454",
       linkGrupo: "https://aqui_fica_o_link_pro_seuGP1.com"
     },
@@ -14,7 +14,7 @@ export const produtos: Produto[] = [
       nome: 'VIP 2',
       descricao: 'Grupo 2 - 60 vídeos e 80 fotos ',
       preco: 'R$29.90',
-      imagem: `${__dirname}\\..\\..\\assets/vip2.jpg`,
+      imagem: `${__dirname}\\..\\assets\\vip2.jpg`,
       linkPagamento: "https://bit.ly/vipaninha2",
       linkGrupo: "https://aqui_fica_o_link_pro_seuGP2.com"
     },
@@ -22,7 +22,7 @@ export const produtos: Produto[] = [
       nome: 'VIP 3',
       descricao: 'Grupo 3 - 154 vídeos e 495 fotos ',
       preco: 'R$79.90',
-      imagem: `${__dirname}\\..\\..\\assets/vip3.jpg`,
+      imagem: `${__dirname}\\..\\assets\\vip3.jpg`,
       linkPagamento: "https://bit.ly/vipaninha3",
       linkGrupo: "https://aqui_fica_o_link_pro_seuGP3.com"
     },
